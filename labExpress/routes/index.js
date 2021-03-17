dcf6ab64f34b1607fa311c374974435929c94db3
kevdam1001 <31449038+kevdam1001@users.noreply.github.com>
2021-03-17 11:40:09 -0600

    lab express done

---
+++ labExpress/routes/index.js
@@ -0,0 +1,55 @@
+const { response } = require('express');
+var express = require('express');
+var router = express.Router();
+const {plantillas} = require('./objetos')
+
+router.get('/', function(req, res, next) {
+  res.json(
+    {Status: 200,
+      plantillas});
+});
+
+router.get('/:id', function(req, res, next) {
+  const item = plantillas.find(c=>c.id === parseInt(req.params.id))
+  if(!item) res.json({Status: 404, Error: 'Not Found'})
+  res.json(
+    {Status: 200,
+      item});
+});
+
+router.post('/', function(req, res, next) {
+
+  if(!req.body.tag || !req.body.date || !req.body.capacidad) res.json({Status: 400, Error: 'Bad Request'});  
+  
+  const plantilla = {
+    id : plantillas.length+1,
+    tag: req.body.tag,
+    date: req.body.date,
+    capacidad: req.body.capacidad
+  }
+  plantillas.push(plantilla);
+  res.json({Status: 200,
+  Response: 'Agregado correctamente'});
+});
+
+
+router.put('/:id', function(req, res, next) {
+  const item = plantillas.find(c=>c.id === parseInt(req.params.id))
+  if(!item) res.json({Status: 404, Error: 'Not Found'})
+  
+  if(req.body.tag) item.tag = req.body.tag;
+  if(req.body.date) item.date = req.body.date;
+  res.json({Status: 204, item});
+
+});
+
+router.delete('/:id', function(req, res, next) {
+  const item = plantillas.find(c=>c.id === parseInt(req.params.id))
+  if(!item) res.json({Status: 404, Error: 'Not Found'})
+
+  const index = plantillas.indexOf(item);
+  plantillas.splice(index,1);
+  res.json({Status: 204, Response: 'Eliminado correctamente'})
+});
+
+module.exports = router;
---
+++ labExpress/routes/objetos.js
@@ -0,0 +1,8 @@
+const plantillas = [
+    {id: 1, tag: "laLiga", date: new Date('2017-01-03')},
+    {id: 2, tag: "premier", date: new Date('2018-03-03')},
+  ];
+
+module.exports={
+    "plantillas" : plantillas
+}
